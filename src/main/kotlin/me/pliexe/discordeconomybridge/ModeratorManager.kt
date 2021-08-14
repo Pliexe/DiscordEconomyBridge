3fe000995de43236570783aa4c26bebee7b7dc66
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2021-08-14 07:59:07 +0200

    discordModerators now doesn't have to be an list and server owners will automatically bypass admin cmds, also added "ignorePermissionsForAdministrators" propety where you can tell the bot if you want people with administrator permission to be able to use the admin commands

--- src/main/kotlin/me/pliexe/discordeconomybridge/ModeratorManager.kt
+++ src/main/kotlin/me/pliexe/discordeconomybridge/ModeratorManager.kt
@@ -1,22 +1,37 @@
 package me.pliexe.discordeconomybridge
 
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.Role
 
 class ModeratorManager(private val main: DiscordEconomyBridge) {
     private val roles = mutableListOf<String>()
 
     fun LoadFromConfig() {
-        if(!main.config.isList("discordModerators")) return
-        main.config.getStringList("discordModerators").forEach {
-            if(it !is String) return
-            if(it.length == 18) {
-                roles.add(it)
+        if(main.config.isList("discordModerators")) {
+            main.config.getStringList("discordModerators").forEach {
+                if(it !is String) return
+                if(it.length == 18) {
+                    roles.add(it)
+                }
             }
+        } else if(main.config.isString("discordModerators")) {
+            val role = main.config.getString("discordModerators")
+            if(role.length == 18)
+                roles.add(role)
         }
+
+
     }
 
-    fun isModerator(_roles: List<Role>): Boolean
+    fun isModerator(member: Member,_roles: List<Role>): Boolean
     {
+        if(member.isOwner) return true
+
+        if(main.config.isBoolean("ignorePermissionsForAdministrators"))
+            if(main.config.getBoolean("ignorePermissionsForAdministrators"))
+                if(member.hasPermission(Permission.ADMINISTRATOR)) return true
+
         for(role in _roles) {
             if(roles.contains(role.id)) {
                 return true
